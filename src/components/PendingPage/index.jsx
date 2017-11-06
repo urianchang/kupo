5fd5fed1140ba3ebd1e6188ad9e03992be728154
Urian <uchang@sightmachine.com>
2017-11-05 23:20:42 -0800

    Retooled Pending page

--- src/components/PendingPage/index.jsx
+++ src/components/PendingPage/index.jsx
@@ -6,7 +6,7 @@ export default class PendingPage extends Component {
   render() {
     return(
       <div className="pending-page">
-        <h1>Under construction, kupo!</h1>
+        <h1 className="pending-text">Under construction, kupo!</h1>
         <img src={moogleWorker} className="moogle-worker" alt="A Moogle Mailman" />
       </div>
     );
