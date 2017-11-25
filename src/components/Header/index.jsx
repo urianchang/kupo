be15691f74fac192b68e5622ee8331960888b27c
Urian <uchang@sightmachine.com>
2017-11-24 20:13:12 -0800

    Refactored header code

---
+++ src/components/Header/index.jsx
@@ -0,0 +1,83 @@
+import React, { Component } from 'react';
+import { NavLink } from 'react-router-dom';
+import './nav.css';
+import './header.css';
+
+const NavbarLinks = [
+  {
+    'url': "/",
+    'exact': true,
+    'title': "Our Story"
+  },
+  {
+    'url': "/wedding",
+    'exact': false,
+    'title': "Wedding"
+  },
+  {
+    'url': "/events",
+    'exact': false,
+    'title': "Events"
+  },
+  {
+    'url': "/rsvp",
+    'exact': false,
+    'title': "RSVP"
+  },
+];
+
+export default class Header extends Component {
+  render() {
+    return(
+      <div className="header">
+        <div className="header-bg"></div>
+        <h1 className="header-text">Angela & Urian</h1>
+        <NavigationBar links={NavbarLinks} />
+      </div>
+    );
+  }
+}
+
+
+class NavigationBar extends Component {
+  constructor(props){
+    super(props);
+    this.makeLinks = this.makeLinks.bind(this);
+  }
+
+  makeLinks(params, i) {
+    if (params['exact']) {
+      return(
+        <li>
+          <NavLink
+            className="navlink"
+            activeClassName="active"
+            exact to={params['url']}>
+            {params['title']}
+          </NavLink>
+        </li>
+      );
+    } else {
+      return(
+        <li>
+          <NavLink
+            className="navlink"
+            activeClassName="active"
+            to={params['url']}>
+            {params['title']}
+          </NavLink>
+        </li>
+      );
+    }
+  }
+  
+  render() {
+    return(
+      <div className="navbar">
+        <ul className="navlinks">
+          {this.props.links.map(this.makeLinks)}
+        </ul>
+      </div>
+    );
+  }
+}
