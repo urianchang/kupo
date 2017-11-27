ae66785038a45708f68231c625990ee08e7746e9
Urian <uchang@sightmachine.com>
2017-11-26 22:26:17 -0800

    Added Photo Album

---
+++ src/components/PhotoAlbum/index.jsx
@@ -0,0 +1,16 @@
+import React, { Component } from 'react';
+
+export default class PhotoAlbum extends Component {
+  render() {
+    return(
+      <div>
+        <blockquote class="imgur-embed-pub" lang="en" data-id="a/dqdfR">
+          <a href="//imgur.com/a/dqdfR">
+            A+U
+          </a>
+        </blockquote>
+        <script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
+      </div>
+    );
+  }
+}
