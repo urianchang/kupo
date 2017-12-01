e179446686a8c4b9a634e7367a539b87db4be85a
Urian <uchang@sightmachine.com>
2017-11-30 22:43:01 -0800

    Added Photo Album

--- src/components/PhotoAlbum/index.jsx
+++ src/components/PhotoAlbum/index.jsx
@@ -1,16 +1,107 @@
 import React, { Component } from 'react';
+import Gallery from 'react-photo-gallery';
+import Lightbox from 'react-images';
+import Measure from 'react-measure';
 
 export default class PhotoAlbum extends Component {
+  constructor() {
+    super();
+    this.state = {
+      width: -1,
+      currentImage: 0,
+    }
+    this.closeLightbox = this.closeLightbox.bind(this);
+    this.openLightbox = this.openLightbox.bind(this);
+    this.gotoNext = this.gotoNext.bind(this);
+    this.gotoPrevious = this.gotoPrevious.bind(this);
+  }
+  openLightbox(event, obj) {
+    this.setState({
+      currentImage: obj.index,
+      lightboxIsOpen: true,
+    });
+  }
+  closeLightbox() {
+    this.setState({
+      currentImage: 0,
+      lightboxIsOpen: false,
+    });
+  }
+  gotoPrevious() {
+    this.setState({
+      currentImage: this.state.currentImage - 1,
+    });
+  }
+  gotoNext() {
+    this.setState({
+      currentImage: this.state.currentImage + 1,
+    });
+  }
   render() {
+    const width = this.state.width;
     return(
-      <div>
-        <blockquote class="imgur-embed-pub" lang="en" data-id="a/dqdfR">
-          <a href="//imgur.com/a/dqdfR">
-            A+U
-          </a>
-        </blockquote>
-        <script async src="//s.imgur.com/min/embed.js" charset="utf-8"></script>
-      </div>
+      <Measure bounds onResize={(contentRect) => this.setState({ width: contentRect.bounds.width })}>
+        {
+        ({measureRef}) => {
+          if (width < 1 ){
+            return <div ref={measureRef}></div>;
+          }
+          let columns = 1;
+          if (width >= 480){
+            columns = 2;
+          }
+          if (width >= 1024){
+            columns = 3;
+          }
+          if (width >= 1824){
+            columns = 4;
+          }
+          return (
+            <div ref={measureRef}>
+              <Gallery
+                photos= {PHOTO_SET}
+                columns={columns}
+                onClick={this.openLightbox} />
+              <Lightbox images={PHOTO_SET}
+                onClose={this.closeLightbox}
+                onClickPrev={this.gotoPrevious}
+                onClickNext={this.gotoNext}
+                currentImage={this.state.currentImage}
+                isOpen={this.state.lightboxIsOpen}
+              />
+            </div>
+          );
+        }
+      }
+      </Measure>
     );
   }
 }
+
+let PHOTO_SET = [
+  {
+    src: 'https://imgur.com/j7UyPkQ.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://imgur.com/eAtwB9H.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://imgur.com/YqdK0f2.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://i.imgur.com/xIyCamg.jpg',
+    width: 4,
+    height: 3
+  },
+  {
+    src: 'https://imgur.com/tx8HTsZ.jpg',
+    width: 4,
+    height: 3
+  },
+];
