c082ddb31066543b3f07f2e96fb8d4f59dcd4c97
Urian <uchang@sightmachine.com>
2017-12-01 22:09:16 -0800

    Made some styling changes for responsiveness

--- src/components/Wedding/index.jsx
+++ src/components/Wedding/index.jsx
@@ -72,8 +72,8 @@ export default class Wedding extends Component {
           <iframe
             title="Google Map of wedding location"
             src="https://www.google.com/maps/embed?pb=!1m14!1m8!1m3!1d14292.921656689214!2d127.7144029!3d26.41604!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x0%3A0x7e236c9ec5087172!2sLazor+Garden+Alivila+Cristea+Church!5e0!3m2!1sen!2sus!4v1511580999709"
-            width="600"
-            height="450"
+            width="500"
+            height="400"
             allowFullScreen>
           </iframe>
         </div>
