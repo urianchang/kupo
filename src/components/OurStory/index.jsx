530294c81c8b402e5c12d6f42d15be5f8b912178
Urian <uchang@sightmachine.com>
2017-12-11 20:10:58 -0800

    Added Chinese version of 'Our Story'

--- src/components/OurStory/index.jsx
+++ src/components/OurStory/index.jsx
@@ -17,6 +17,18 @@ export default class OurStory extends Component {
             We would love to celebrate our marriage with you, our family and friends, because each of you have made an impact on our lives; and without you, we wouldn't be where we are today. From here to there, let us fly with time and make some happy memories.
           </p>
         </div>
+        <h1 className="important-text">我們的故事</h1>
+        <div className="left-aligned">
+          <p>
+            我們兩個是在UCLA大一時，一堂叫做「What is Time」的課堂上認識的。這堂課打開了我們的眼界，讓我們了解到原來時間有這麼多種方式可以被追蹤及衡量。它可以用我們第一次從布魯恩步道邊走邊聊回宿舍的步伐來計算；它還可以用 當我們兩個住在不同城市時 ，為了相聚多次飛往對方懷抱 的距離來計算；它也可以用我們一起出國冒險旅遊的次數來計算。
+          </p>
+          <p>
+            時間可以從我們因為對方微笑因而產生的眼角笑紋上看到。它可以從我們和親朋好友相聚時的笑聲中聽到。它可以從我們因為心愛寵物的離開而產生的痛苦中感受出來。它可以從Urian原本比較清淡的口味慢慢轉變成和Angela一樣的重口味中品嚐出來。從我們經常忘記我們已經交往多久的次數到我們經常一起計劃未來夢想的次數， 我們都可以在時間的軌跡中收集點點滴滴珍貴的回憶。
+          </p>
+          <p>
+            親愛的家人和朋友，我們希望和您一起慶祝我們的婚禮，因為您們每一位都在我們的生命中留下深刻且重要的印記。沒有您們，就不會成就今日的我們。從此地到彼處，讓我們一起和時間飛行、共同創造難忘歡樂的回憶!
+          </p>
+        </div>
       </div>
     );
   }
