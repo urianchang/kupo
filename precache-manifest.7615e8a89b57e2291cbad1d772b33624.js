55d55b4df3380e7f7cef0ddb5199edfba55ba21f
Urian <uchang@sightmachine.com>
2019-01-28 23:39:11 -0800

    Updates

---
+++ precache-manifest.7615e8a89b57e2291cbad1d772b33624.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "2c2e779ea4a52a8ee4c01df2fb94a440",
+    "url": "/kupo/static/media/tree-branch.2c2e779e.svg"
+  },
+  {
+    "revision": "592e00a271069223e92e7e7bd1d06561",
+    "url": "/kupo/static/media/moogle_worker.592e00a2.png"
+  },
+  {
+    "revision": "6c14124e5ef0a331cc6e5411243288db",
+    "url": "/kupo/static/media/champagne.6c14124e.png"
+  },
+  {
+    "revision": "2d89582f7a68faf7e016",
+    "url": "/kupo/static/js/runtime~main.2d89582f.js"
+  },
+  {
+    "revision": "6375827a8de4b3c33360",
+    "url": "/kupo/static/js/main.6375827a.chunk.js"
+  },
+  {
+    "revision": "7cd11f1b44ab9c194535",
+    "url": "/kupo/static/js/1.7cd11f1b.chunk.js"
+  },
+  {
+    "revision": "6375827a8de4b3c33360",
+    "url": "/kupo/static/css/main.ea63d045.chunk.css"
+  },
+  {
+    "revision": "f40cf403107e96ec44f7549abcd3abc3",
+    "url": "/kupo/index.html"
+  }
+];
